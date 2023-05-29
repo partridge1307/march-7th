55da9abb8c28c1d19d62fad71e07c5f71e15ef0b
Partridge <anhduc130703@gmail.com>
2023-05-29 18:04:43 +0700

    update invite link

--- src/notify.js
+++ src/notify.js
@@ -5,7 +5,7 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setTitle("Please ignore this if you don't get 'This bot has not been verified'")
       .setURL(
-        'https://discord.com/api/oauth2/authorize?client_id=1112647992160292915&permissions=32767&scope=bot%20applications.commands'
+        'https://discord.com/api/oauth2/authorize?client_id=1109375320718594078&permissions=32767&scope=bot%20applications.commands'
       )
       .setDescription(
         "That means I'm reached to 100 servers so I can not join any more server. Please add Herta (my friend) if you like me by click the title or add other bot. My master will update both me and her"
